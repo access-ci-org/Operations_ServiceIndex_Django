7ed0b2ed8e791f06d3673597f0ec07482f1d81d0
jyssy <jessel@phantomlandscapes.com>
2025-07-31 16:41:09 -0400

    updating settings errors

--- Operations_ServiceIndex_Django/Operations_ServiceIndex_Django/settings.py
+++ Operations_ServiceIndex_Django/Operations_ServiceIndex_Django/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-from Operations_ServiceIndex_Django import services
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
