a9625e84b59a4fd234a13b00fc7ef59e574e8177
jyssy <jessel@phantomlandscapes.com>
2026-01-09 14:25:09 -0500

    making migration and models changes to reflect new schema owner needs

--- Operations_ServiceIndex_Django/services/migrations/0001_initial.py
+++ Operations_ServiceIndex_Django/services/migrations/0001_initial.py
@@ -18,10 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('tier', models.IntegerField()),
                 ('description', models.CharField(max_length=512)),
-            ],
-            options={
-                'db_table': '"serviceindex"."availability"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='Event',
@@ -30,10 +27,7 @@ class Migration(migrations.Migration):
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('name', models.CharField(max_length=128)),
                 ('description', models.CharField(max_length=1024)),
-            ],
-            options={
-                'db_table': '"serviceindex"."event"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='Host',
@@ -47,10 +41,7 @@ class Migration(migrations.Migration):
                 ('note', models.CharField(blank=True, max_length=2048)),
                 ('host_last_verified', models.DateField(blank=True, null=True)),
                 ('availability', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='services.availability')),
-            ],
-            options={
-                'db_table': '"serviceindex"."host"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='Service',
@@ -68,20 +59,14 @@ class Migration(migrations.Migration):
                 ('otp', models.BooleanField()),
                 ('nagios', models.BooleanField()),
                 ('deprecated', models.BooleanField(default=False)),
-            ],
-            options={
-                'db_table': '"serviceindex"."service"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='Site',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('site', models.CharField(max_length=256)),
-            ],
-            options={
-                'db_table': '"serviceindex"."site"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='Staff',
@@ -92,20 +77,14 @@ class Migration(migrations.Migration):
                 ('phone', models.CharField(blank=True, max_length=64)),
                 ('last_name', models.CharField(max_length=64)),
                 ('deleted', models.BooleanField(default=False)),
-            ],
-            options={
-                'db_table': '"serviceindex"."staff"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='Support',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('hours', models.CharField(max_length=256)),
-            ],
-            options={
-                'db_table': '"serviceindex"."support"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='LogEntry',
@@ -115,10 +94,7 @@ class Migration(migrations.Migration):
                 ('username', models.CharField(max_length=16)),
                 ('msg', models.CharField(max_length=1024)),
                 ('service', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='services.service')),
-            ],
-            options={
-                'db_table': '"serviceindex"."logentry"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='Link',
@@ -127,10 +103,7 @@ class Migration(migrations.Migration):
                 ('url', models.URLField(max_length=512)),
                 ('description', models.CharField(max_length=1024)),
                 ('service', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='services.service')),
-            ],
-            options={
-                'db_table': '"serviceindex"."link"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='HostEventStatus',
@@ -139,10 +112,7 @@ class Migration(migrations.Migration):
                 ('status', models.CharField(choices=[('unchecked', 'unchecked'), ('in_progress', 'in progress'), ('compliant', 'compliant'), ('na', 'N/A')], max_length=32)),
                 ('event', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='services.event')),
                 ('host', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='services.host')),
-            ],
-            options={
-                'db_table': '"serviceindex"."hosteventstatus"',
-            },
+            ]
         ),
         migrations.CreateModel(
             name='HostEventLog',
@@ -154,10 +124,7 @@ class Migration(migrations.Migration):
                 ('status', models.CharField(choices=[('unchecked', 'unchecked'), ('in_progress', 'in progress'), ('compliant', 'compliant'), ('na', 'N/A')], max_length=32)),
                 ('event', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='services.event')),
                 ('host', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='services.host')),
-            ],
-            options={
-                'db_table': '"serviceindex"."hosteventlog"',
-            },
+            ]
         ),
         migrations.AddField(
             model_name='host',
@@ -196,9 +163,6 @@ class Migration(migrations.Migration):
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('username', models.CharField(max_length=16)),
                 ('service', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='services.service')),
-            ],
-            options={
-                'db_table': '"serviceindex"."editlock"',
-            },
+            ]
         ),
     ]
